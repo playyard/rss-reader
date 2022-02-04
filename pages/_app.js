52880ace424ebe2b1e352b6a07f1279e52a76405
playyard <90937279+playyard@users.noreply.github.com>
2022-02-04 21:46:37 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
     <div>
       <nav className="p-6 flex justify-between items-center">
         <Link href="/">
-          <a class="bg-pink-50 hover:bg-pink-200 border-pink-400 border-b-2">Home</a>
+          <a class="hover:bg-pink-200 border-pink-400 border-b-2">Home</a>
         </Link>
       </nav>
       <Component {...pageProps} />
