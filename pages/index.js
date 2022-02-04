df015f163fd80eb3acce3a454137e19313d790c9
playyard <90937279+playyard@users.noreply.github.com>
2022-02-04 22:25:33 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="px-0 py-12 max-w-xl mx-auto">
-        <h1 className="font-bold text-5xl underline-pink-500 mb-5">WhatsApps?</h1>
+        <h1 className="font-bold text-5xl mb-5">WhatsApps?</h1>
       <div className="grid grid-cols-2 gap-4">
         {FEEDS.map((feed) => (
           <Link key={feed.slug} href={`/feeds/${feed.slug}`}>
-            <a className="p-4 border border-gray-200 hover:border-gray-500 rounded-lg">
+            <a className="p-4 border border-gray-200 hover:border-gray-500 hover:drop-shadow-xl rounded-lg">
               {feed.title}
             </a>
           </Link>
